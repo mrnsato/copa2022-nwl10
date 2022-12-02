59eb7703ef8b9999a8783322c8f9fb7956d88e96
mrnsato <mellissarochasantos@outlook.com>
2022-12-02 18:34:16 -0300

    descrição commit

--- main.js
+++ main.js
@@ -133,13 +133,13 @@ document.querySelector("#cards").innerHTML =
     "05/12",
     "segunda",
     createGame("croatia", "12:00", "japan") +
-      createGame("southkorea", "16:00", "aconfirmar")
+      createGame("southkorea", "16:00", "brazil")
   ) +
   createCard(
     "06/12",
     "terça",
     createGame("morroco", "12:00", "spain") +
-      createGame("portugal", "16:00", "aconfirmar")
+      createGame("portugal", "16:00", "stwizerland")
   ) +
   createCard(
     "09/12",
